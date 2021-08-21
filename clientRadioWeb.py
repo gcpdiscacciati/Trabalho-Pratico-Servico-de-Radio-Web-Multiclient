88fcc00a336029ecb7fd52f144dc92a08cdc35ee
Giovanne Discacciati <gidiscacciati@gmail.com>
2021-08-21 16:10:18 -0300

    múltiplos clientes simultâneos (sem travar) - added tratamento de exceções

--- clientRadioWeb.py
+++ clientRadioWeb.py
@@ -5,10 +5,14 @@ import socket
 import pyaudio
 import threading
 
+#Alterar para o IP do servidor
 serverName = '127.0.0.1'
 serverPort = 12000
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+try:
+    clientSocket.connect((serverName, serverPort))
+except ConnectionRefusedError:
+    print("Conexão não estabelecida com o servidor. Tente novamente")
 
 p = pyaudio.PyAudio()
 
@@ -19,13 +23,19 @@ CHUNK = 2048
 
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
 
-content = clientSocket.recv(CHUNK)
-
-while content:
-    stream.write(content)   # "Player" de áudio
+try:
     content = clientSocket.recv(CHUNK)
 
-print("Audio executado")
+    while content:
+        stream.write(content)   # "Player" de áudio
+        content = clientSocket.recv(CHUNK)
+except KeyboardInterrupt:
+    print("Conexão com o servidor encerrada")
+except ConnectionResetError:
+    print("Conexão encerrada pelo servidor")
+except OSError:
+    pass
+
 
 stream.close()
 p.terminate()
